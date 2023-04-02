7bb0e31a3865062c15b29655a5b0d9b61f1449b4
GabyFCE <duartegabriel2017@gmail.com>
2023-04-02 00:57:22 -0300

    Add project files.

---
+++ Ejercicios.Ejercicio6/Program.cs
@@ -0,0 +1,16 @@
+﻿ Console.WriteLine("Escribe algo y presiona Ctrl+Shift+F para finalizar el programa.");
+
+ while (true)
+ {
+    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+    if (keyInfo.Modifiers == (ConsoleModifiers.Control | ConsoleModifiers.Shift) && keyInfo.Key == ConsoleKey.F)
+    {
+        Console.WriteLine("\nPrograma finalizado.");
+        break;
+    }
+    else
+    {
+        Console.Write(keyInfo.KeyChar);
+    }
+ }
+
